fb4dad601c8540588dc86c175649e006d06d42a7
peccu <peccul@gmail.com>
2018-05-18 00:14:54 +0900

    add average page

---
+++ src/views/Average.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="average">
+    <h1>平均想定年収</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Average'
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
