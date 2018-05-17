f23762ec8cbf157675afd6108f3083fa28bbc19e
peccu <peccul@gmail.com>
2018-05-17 22:42:43 +0900

    Vcg: Update live demo

---
+++ precache-manifest.091f0783b5fada1a2fc60015d02f1586.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdf9a9e4bf2a92f061f7",
+    "url": "/js/vendors~app.fdf9a9e4.js"
+  },
+  {
+    "revision": "03c4087f89b78e02742e",
+    "url": "/js/app.03c4087f.js"
+  },
+  {
+    "revision": "d4f99a45475ed65481bbbff51ca98db1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03c4087f89b78e02742e",
+    "url": "/css/app.e5cfdd82.css"
+  }
+];
