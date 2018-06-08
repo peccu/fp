3f5e734c73ece89f0f118ce0095d6fe584041cfb
peccu <peccul@gmail.com>
2018-06-08 13:28:59 +0900

    add vue-analytics

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import './registerServiceWorker'
 import '@/../semantic/dist/semantic.css'
 import '@/../semantic/dist/semantic.js'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
 
@@ -15,6 +16,11 @@ function gtag () {
 router.beforeEach((to, from, next) => {
   gtag('config', 'UA-65053410-2', {'page_path': to.path})
   next()
+})
+
+Vue.use(VueAnalytics, {
+  id: 'UA-65053410-2',
+  router
 })
 
 new Vue({
